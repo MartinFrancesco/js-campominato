0d9c4b4d271210a023ea0ff836abedc954d039eb
Francesco <francescomartinmail@gmail.com>
2020-10-23 15:41:42 +0200

    Ex

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //ESERCIZIO CAMPO-MINATO
 //Il computer deve generare 16 numeri casuali (bombe) tra 1 e 100.
 //I numeri non possono essere duplicati
-
 var arrBombs = [];
+var arrAttempts =
 
 for (var i = 0; i < 16; i++) {
   var bomb = bombGenerator();
@@ -14,16 +14,32 @@ for (var i = 0; i < 16; i++) {
 
 console.log(arrBombs);
 
+//In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+//L'utente non può inserire più volte lo stesso numero.
+for (var i = 0; i < 3; i++) {
+  var guess = guessUser();
+
+  if (guess > 100) {
+    alert("Errore!");
+  } else if (guess === 0){
+    alert("Errore!");
+  }
+}
 
 
 
 
 
-
-//Definizione funzioni
-
+//DEFINIZIONE FUNZIONI
+//Generatore di bombe
 function bombGenerator() {
-  var bombsGenerator = Math.floor( Math.random() * (100 - 1 + 1)) + 1;
+  var x = Math.floor( Math.random() * (100 - 1 + 1)) + 1;
 
-  return bombsGenerator;
+  return x;
+}
+//Tentativi utente
+function guessUser() {
+  var x = parseInt( prompt("Inserisci un numero, tenta la fortuna!"));
+
+  return x;
 }
