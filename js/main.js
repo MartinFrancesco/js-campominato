c7f18cd131ad3ba0ae9c6a2f60814fb5b93817c2
Francesco <francescomartinmail@gmail.com>
2020-10-25 23:40:58 +0100

    Completed

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 //I numeri non possono essere duplicati
 var arrBombs = [];
 
-for (var i = 0; i < 3; i++) {
+for (var i = 1; i < 100; i++) {
   var bomb = bombGenerator();
 
   if (bomb !== arrBombs[i]) {
@@ -13,41 +13,31 @@ for (var i = 0; i < 3; i++) {
 
 console.log(arrBombs);
 
-//In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+//In seguito deve chiedere all'utente (84) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 //L'utente non può inserire più volte lo stesso numero.
 //Se il numero è presente nella lista dei numeri generati (bombe), la partita termina, altrimenti si continua chiedendo all'utente un altro numero.
 //La partita termina quando il giocatore inserisce un numero "vietato" o raggiunge il numero massimo possibile di numeri consentiti.
-var attempts = 3;
+var attempts = 84;
 var games = [];
 
 for (var i = 0; i < attempts; i++) {
   var guess = guessUser();
 
-  if (guess !== games[i]) {
-    games.push(guess);
-  } else if (guess === 0){
-    alert("Errore!");
-  } else if (guess > 100){
-    alert("Errore!");
-  } else if (guess === games[i]){
-    alert("Errore!");
-  } else if (guess === arrBombs[i]) {
-    alert("GAME OVER!");
-  } else if (games.length == attempts) {
-    alert("HAI VINTO!!!");
+  //Condizioni e regole
+  while (isNaN(guess) || (guess === 0) || (guess > 100) || (games.includes(guess))) {
+    var guess = parseInt( prompt("Non stai rispettando le regole! Riprova."));
+  }
+
+  if (! arrBombs.includes(guess)) {
+     games.push(guess);
+     console.log(games);
+
+  } else {
+      alert("GAME OVER!");
   }
 }
 
-switch (guess !== games[i]) {
-  case expression:
-
-    break;
-  default:
-
-}
-
-console.log(games);
-
+alert("HAI VINTO!");
 
 //DEFINIZIONE FUNZIONI
 //Generatore di bombe
@@ -56,6 +46,7 @@ function bombGenerator() {
 
   return x;
 }
+
 //Tentativi utente
 function guessUser() {
   var x = parseInt( prompt("Inserisci un numero, tenta la fortuna!"));
